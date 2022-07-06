c55859a2b2418ffc0f5782abf8594cceb2aa6174
ArenRitz <aren.dolbakian@gmail.com>
2022-07-06 16:22:39 -0400

    Added ability to move snake using WASD

--- input.js
+++ input.js
@@ -1,4 +1,8 @@
-const setupInput = function () {
+let connection;
+
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -11,7 +15,20 @@ const handleUserInput = function (data) {
   // your code here
   if (data === '\u0003') {
     process.exit();
+  } else if (data === '\u0077') {
+    console.log("w");
+    connection.write("Move: up");
+  } else if (data === '\u0061') {
+    console.log("a");
+    connection.write("Move: left");
+  } else if (data === '\u0073') {
+    console.log("s");
+    connection.write("Move: down");
+  } else if (data === '\u0064') {
+    console.log("d");
+    connection.write("Move: right");
   }
+
 };
 
 module.exports = {setupInput,}
